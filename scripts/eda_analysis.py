eac89473305a7e37585177fcee3a525934caa8e6
Amen Zelealem <amenzelealemtadese@gmail.com>
2025-01-23 17:35:07 +0300

    Completed categorical_distribution

--- scripts/eda_analysis.py
+++ scripts/eda_analysis.py
@@ -135,7 +135,7 @@ class CreditRiskAnalysis:
         plt.suptitle('Distribution of Numeric Variables', fontsize=20, fontweight='bold', y=1.02)
         plt.show()
 
-        # Function to plot skewness for each numerical feature
+    # Function to plot skewness for each numerical feature
     def plot_skewness(self):
         df = self.df.select_dtypes(include='number')
         skewness = df.skew().sort_values(ascending=False)
@@ -163,4 +163,48 @@ class CreditRiskAnalysis:
             plt.text(index, value, f'{value:.2f}', ha='center', va='bottom', fontsize=10, color='black')
 
         plt.tight_layout()
+        plt.show()
+
+
+    def plot_categorical_distribution(self):
+        """
+        Function to plot the distribution of categorical features in a DataFrame and 
+        display the count values on top of each bar.
+        
+        Parameters:
+        -----------
+        df : pandas.DataFrame
+            The DataFrame containing the dataset to be analyzed.
+        """
+        # Select categorical columns
+        categorical_cols = self.df.select_dtypes(include=['object', 'category']).columns
+        cols_with_few_categories = [col for col in categorical_cols if self.df[col].nunique() <= 10]
+
+        # Set up the grid for subplots
+        num_cols = len(cols_with_few_categories)
+        num_rows = (num_cols + 1) // 2  # Automatically determine the grid size
+        
+        fig, axes = plt.subplots(num_rows, 2, figsize=(15, num_rows * 5))
+        axes = axes.flatten()
+
+        for i, col in enumerate(cols_with_few_categories):
+            ax = sns.countplot(data=self.df, x=col, ax=axes[i], hue=col, legend=False, palette="pastel", edgecolor='black')
+            axes[i].set_title(f'Distribution of {col}', fontsize=16, fontweight='bold')
+            axes[i].tick_params(axis='x', rotation=45)
+            axes[i].set_xlabel('')
+            axes[i].set_ylabel('Frequency', fontsize=14)
+
+            # Add count labels to the top of the bars
+            for p in ax.patches:
+                ax.annotate(f'{int(p.get_height())}', 
+                            (p.get_x() + p.get_width() / 2., p.get_height()), 
+                            ha='center', va='bottom', fontsize=12, color='black', 
+                            xytext=(0, 5), textcoords='offset points')
+
+        # Remove any empty subplots
+        for j in range(i + 1, len(axes)):
+            fig.delaxes(axes[j])
+        
+        plt.tight_layout()
+        plt.suptitle('Categorical Feature Distributions', fontsize=18, fontweight='bold', y=1.02)
         plt.show()
